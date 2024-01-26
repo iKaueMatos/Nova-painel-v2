37d5617425d9a61f14836043d996ceb6c772dfec
Kauê Matos <98132837+iKaueMatos@users.noreply.github.com>
2024-01-25 23:05:07 -0300

    Update states.ts

--- src/service/external/ibge-states/states.ts
+++ src/service/external/ibge-states/states.ts
@@ -1,19 +1,19 @@
-import { IBGE_STATES } from "@/constants/BASE_API";
-import axios, { AxiosResponse } from "axios";
+// import { IBGE_STATES } from "@/constants/BASE_API";
+// import axios, { AxiosResponse } from "axios";
 
-interface Request {
-}
+// interface Request {
+// }
 
-export const fetchStates = async (request: Request): Promise<any> => {
-  try {
-    const { data }: AxiosResponse = await axios.get(IBGE_STATES);
-    if (data.response.status >= 400) {
-      throw new Error("Erro ao consultar API do IBGE");
-    }
+// export const fetchStates = async (request: Request): Promise<any> => {
+//   try {
+//     const { data }: AxiosResponse = await axios.get(IBGE_STATES);
+//     if (data.response.status >= 400) {
+//       throw new Error("Erro ao consultar API do IBGE");
+//     }
 
-    return data;
-  } catch (error) {
-    console.error("Ocorreu um erro durante a solicitação da API:", error);
-    throw error;
-  }
-};
+//     return data;
+//   } catch (error) {
+//     console.error("Ocorreu um erro durante a solicitação da API:", error);
+//     throw error;
+//   }
+// };
